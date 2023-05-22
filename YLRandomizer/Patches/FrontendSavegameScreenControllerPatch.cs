99aeee42df931680d7cb49c6b0344678b0e19d9e
SunnyBat <Sunnybat@yahoo.com>
2023-05-21 18:06:33 -0700

    This works better

---
+++ YLRandomizer/Patches/FrontendSavegameScreenControllerPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using UnityEngine.EventSystems;
+using YLRandomizer.Data;
+using YLRandomizer.Logging;
+using YLRandomizer.Randomizer;
+
+namespace YLRandomizer.Patches
+{
+    [HarmonyPatch(typeof(FrontendSavegameScreenController), "OnSubmit")]
+    public class FrontendSavegameScreenController_OnSubmit
+    {
+        [HarmonyPrefix]
+        public static bool SometimesReplace(BaseEventData eventData)
+        {
+            ManualSingleton<ILogger>.instance.Debug($"FrontendSavegameScreenController_OnSubmit.SometimesReplace()");
+            if (ManualSingleton<IRandomizer>.instance != null)
+            {
+                ManualSingleton<IUserMessages>.instance.AddMessage("Not connected to Archipelago, cannot load save!");
+                ManualSingleton<IUserMessages>.instance.AddMessage("Connect before loading the save.");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
