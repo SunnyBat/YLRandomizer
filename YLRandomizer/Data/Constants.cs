f160e7a6b9febf9a10dae46851659f8d7d2ac592
SunnyBat <Sunnybat@yahoo.com>
2023-06-11 16:26:56 -0700

    Block unlocking worlds without adequate pagies

---
+++ YLRandomizer/Data/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace YLRandomizer.Data
+{
+    public class Constants
+    {
+        /// <summary>
+        /// The order in which book world indeces (eg excluding Hivory Towers) are ordered in SavegameManager.instance.savegame.worlds.
+        /// </summary>
+        public static readonly int[] WorldIndexOrder = new int[] { 2, 5, 6, 4, 3 };
+
+        /// <summary>
+        /// The names of all of the world unlock event names. These correspond to the same worlds referenced in <see cref="WorldIndexOrder"/>.
+        /// </summary>
+        public static readonly string[] UnlockEventNames = new string[] { "UnlockTribalstack", "UnlockGlacier", "UnlockMarsh", "UnlockCasino", "UnlockGalleon" };
+    }
+}
---
+++ YLRandomizer/Patches/ShowQuestionPatches.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using ParadoxNotion;
+using System;
+using System.Linq;
+using YLRandomizer.Data;
+using YLRandomizer.Logging;
+
+namespace YLRandomizer.Patches
+{
+    [HarmonyPatch(typeof(ShowQuestion), nameof(ShowQuestion.OnConversationOptionPicked))]
+    public class ShowQuestion_OnConversationOptionPicked
+    {
+        public static string LastClosedConversation;
+
+        [HarmonyPrefix]
+        public static bool SometimesReplace(Conversation.DialogOption option, ShowQuestion __instance)
+        {
+            ManualSingleton<ILogger>.instance.Info($"ShowQuestion_OnConversationOptionPicked.Postfix(): {option}");
+            if (option == Conversation.DialogOption.Accept && Constants.UnlockEventNames.Contains(LastClosedConversation))
+            {
+                // TODO Calculate if player has enough pagies to unlock and expand all previous worlds plus this one before allowing
+                // TODO Change from IUserMessages to a custom Conversation and use QueueConversation in ShowConversation to display it
+                ManualSingleton<IUserMessages>.instance.AddMessage("You must have enough pagies to unlock all of Tribalstack Tropics first.");
+                var showQuestionClass = typeof(ShowQuestion);
+                var sendEventFunction = showQuestionClass.GetMethod("SendEvent", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic, null, new Type[] { typeof(EventData) }, null);
+                sendEventFunction.Invoke(__instance, new object[] { new EventData(__instance.OptionDeclined.value) });
+                return false;
+            }
+            return true;
+        }
+    }
+}
