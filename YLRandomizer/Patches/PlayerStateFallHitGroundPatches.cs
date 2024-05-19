8e553f1ffb057756204ab16f0ca191ac9c1a098c
SunnyBat <Sunnybat@yahoo.com>
2024-05-18 20:49:44 -0700

    More DeathLinks

---
+++ YLRandomizer/Patches/PlayerStateFallHitGroundPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using YLRandomizer.Logging;
+using YLRandomizer.Randomizer;
+
+namespace YLRandomizer.Patches
+{
+    [HarmonyPatch(typeof(PlayerStateFallHitGround), nameof(PlayerStateFallHitGround.StateEnter))]
+    public class PlayerStateFallHitGround_StateEnter
+    {
+        [HarmonyPostfix]
+        public static void Postfix(CharState fromState, PlayerStateFallHitGround __instance)
+        {
+            ManualSingleton<ILogger>.instance.Debug($"PlayerStateFallHitGround_StateEnter(): {fromState.Name}");
+            var mIsDeadfromFall = (bool)typeof(PlayerStateFallHitGround)
+                .GetField("mIsDeadfromFall", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .GetValue(__instance);
+            if (mIsDeadfromFall)
+            {
+                ManualSingleton<IRandomizer>.instance.SendDeathLink("Hit the ground a little too fast");
+            }
+        }
+    }
+}
---
+++ YLRandomizer/Patches/PlayerStateSwimUnderwaterDiePatches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using YLRandomizer.Logging;
+using YLRandomizer.Randomizer;
+
+namespace YLRandomizer.Patches
+{
+    [HarmonyPatch(typeof(PlayerStateSwimUnderwaterDie), nameof(PlayerStateSwimUnderwaterDie.StateEnter))]
+    public class PlayerStateSwimUnderwaterDie_StateEnter
+    {
+        [HarmonyPostfix]
+        public static void Postfix(CharState fromState)
+        {
+            ManualSingleton<ILogger>.instance.Debug($"PlayerStateSwimUnderwaterDie_StateEnter(): {fromState.Name}");
+            ManualSingleton<IRandomizer>.instance.SendDeathLink("Stayed underwater for too long");
+        }
+    }
+}
